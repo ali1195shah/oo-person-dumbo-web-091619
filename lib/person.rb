60660ffb439e91cbae0bdb1dc56faf85f3655657
Ali Shah <ali1195shah@gmail.com>
2019-09-18 21:09:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -72,10 +72,10 @@ class Person
       return "♪ another one bites the dust ♫"
     end
     
-    def call_friend(person_instance)
+    def call_friend(person)
     self.happiness += 3
-    person_instance.happiness += 3
-    "Hi #{person_instance.name}! It's #{self.name}. How are you?"
+    person.happiness += 3
+    "Hi #{person.name}! It's #{self.name}. How are you?"
   end
     
     
